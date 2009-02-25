efa892e0c03e28271ddb8ceab6a44d354e04667e
ahkdiep <ahkdiep@f1d5efd2-00ae-11de-802b-177316e4ac06>
2009-02-25 20:20:57 +0000

    r187092@www:  root | 2009-02-25 13:19:35 -0700  tag release 1.0.5

---
+++ tags/python-ctags-1.0.5/src/examples/example.py
@@ -0,0 +1,38 @@
+import ctags
+from ctags import CTags, TagEntry
+import sys
+
+try:
+    tagFile = CTags('tags')
+except:
+    sys.exit(1)
+    
+   
+entry = TagEntry()
+status = tagFile.setSortType(ctags.TAG_SORTED)
+status = tagFile.first(entry)
+
+print tagFile['name']
+print tagFile['author']
+print tagFile['format']
+if status:
+    print entry['name']
+    print entry['kind']
+    
+if tagFile.find(entry, 'find', ctags.TAG_PARTIALMATCH | ctags.TAG_IGNORECASE):
+    print 'found'
+    print entry['lineNumber']
+    print entry['pattern']
+    print entry['kind']
+
+status = tagFile.findNext(entry)
+if status:
+    print entry['lineNumber']
+    print entry['pattern']
+    print entry['kind']
+    
+if tagFile.next(entry):
+    print entry['lineNumber']
+    print entry['pattern']
+    print entry['kind']
+    
